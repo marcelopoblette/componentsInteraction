4611efe04b706569f8338caf1216ec034860a93a
Marcelo Poblete <marcelopoblette@gmail.com>
2020-02-28 06:24:16 -0300

    Interaction Parent-Child finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'componentsInteraction';
+  public name = "Marcelo";
+  public message = "";
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  @Input('parentData') public name;
+  @Output() public childEvent = new EventEmitter();
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  fireEvent(){
+    this.childEvent.emit('Hey Father!');
   }
 
 }
